0b8e3d98c95599a23a9d9dc4b4dfd70c7feae6a8
Hanif <hanif.hakim@spesolution.com>
2023-10-26 19:59:20 +0700

    First Init

---
+++ src/main/java/com/hakimfauzi/groupreminderbot/GroupReminderBotApplication.java
@@ -0,0 +1,13 @@
+package com.hakimfauzi.groupreminderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupReminderBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupReminderBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakimfauzi/groupreminderbot/GroupReminderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakimfauzi.groupreminderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupReminderBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
